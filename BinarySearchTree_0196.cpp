088c52957a52ddd2c99b5fded231bc4adac8a8aa
Wirayudhas <arturwirayuda@gmail.com>
2024-06-13 12:03:11 +0700

    membuat void preorder

--- BinarySearchTree_0196.cpp
+++ BinarySearchTree_0196.cpp
@@ -87,4 +87,19 @@ public:
 		}
 	}
 
+	void preorder(Node* ptr)
+	{
+		if (ROOT == nullptr)
+		{
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+
 	
